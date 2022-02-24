b3fcdd11fa3341021cc9b86db9ac82888215efa4
Anikin Luke Abales <47096239+ph-fox@users.noreply.github.com>
2022-02-24 21:01:56 +0800

    Update NinjaSwitch.ino

--- NinjaSwitch.ino
+++ NinjaSwitch.ino
@@ -22,6 +22,8 @@ String responseHTML = ""
   "<title>Ninja</title></head><body>"
   "<center><br><br><br>"
   "<h2>Ninja Switch</h2>"
+  "<a class='grn' href=/bon>Built in Turn On</a>"
+  "<a class='red' href=/boff>Built in Turn Off</a>"
   "<a class='grn' href=/1on>1 Turn On</a>"
   "<a class='red' href=/1off>1 Turn Off</a>"
   "<a class='grn' href=/2on>2 Turn On</a>"
@@ -38,6 +40,16 @@ String responseHTML = ""
   "</body>"
   "<style>body{user-select: none;background-color: #303030;margin: 0;height: 100vh;}center{background-color: #151515;height: 100%;}.grn{background-color: green;}.red{background-color: maroon;}h2{color: cyan;font-family: sans-serif;margin: 0;}a{user-select: none;color: cyan;font-family: sans-serif;text-decoration: none;display: block;padding: 10px;margin: 5px;width: 20%;border-radius: 10px;border: 1px solid cyan;}a:active{background-color: rgb(0,0,0, 0.1);}@media (max-width: 900px){a{width: 60%;}}</style>"
   "</html>";
+
+String led_onBuilt(){
+  digitalWrite(LED_BUILTIN, LOW);
+  return responseHTML;
+}
+
+String led_offBuilt(){
+  digitalWrite(LED_BUILTIN, HIGH); 
+  return responseHTML;
+}
 
 String led_on1(){
   digitalWrite(led_1, LOW);
@@ -100,11 +112,19 @@ String led_off6(){
 }
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
   pinMode(led_1, OUTPUT);
+  pinMode(led_2, OUTPUT);
+  pinMode(led_3, OUTPUT);
+  pinMode(led_4, OUTPUT);
+  pinMode(led_5, OUTPUT);
+  pinMode(led_6, OUTPUT);
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
   WiFi.softAP(AP_name);
 
+  // if DNSServer is started with "*" for domain name, it will reply with
+  // provided IP to all DNS request
   dnsServer.start(DNS_PORT, "*", apIP);
   webServer.on("/1on",[]() { webServer.send(200, "text/html", led_on1()); });
   webServer.on("/1off",[]() { webServer.send(200, "text/html", led_off1()); });
@@ -118,6 +138,8 @@ void setup() {
   webServer.on("/5off",[]() { webServer.send(200, "text/html", led_off5()); });
   webServer.on("/6on",[]() { webServer.send(200, "text/html", led_on6()); });
   webServer.on("/6off",[]() { webServer.send(200, "text/html", led_off6()); });
+  webServer.on("/bon",[]() { webServer.send(200, "text/html", led_onBuilt()); });
+  webServer.on("/boff",[]() { webServer.send(200, "text/html", led_offBuilt()); });
   webServer.onNotFound([]() {
     webServer.send(200, "text/html", responseHTML);
   });
